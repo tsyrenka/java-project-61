1e001eca684a50d0b417c9d111fa67ac29279db4
tsyrenka <cyrenlodoev@mail.ru>
2024-05-01 13:10:18 +0300

    add commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,18 @@
 plugins {
-    id("java")
+    id("application")
 }
-
+application{
+    mainClass.set("hexlet.code.App")
+}
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
-
 tasks.test {
     useJUnitPlatform()
 }
